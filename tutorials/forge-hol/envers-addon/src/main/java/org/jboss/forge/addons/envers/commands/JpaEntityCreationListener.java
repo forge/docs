7ae021f332cfdb90cb753dd28bf51b2c69014249
Ivan St. Ivanov <ivan_st_ivanov@yahoo.com>
2014-11-08 00:43:15 +0200

    Finalized the developing Forge chapter

--- tutorials/forge-hol/envers-addon/src/main/java/org/jboss/forge/addons/envers/commands/JpaEntityCreationListener.java
+++ tutorials/forge-hol/envers-addon/src/main/java/org/jboss/forge/addons/envers/commands/JpaEntityCreationListener.java
@@ -2,6 +2,7 @@ package org.jboss.forge.addons.envers.commands;
 
 import org.jboss.forge.addon.configuration.Configuration;
 import org.jboss.forge.addon.configuration.facets.ConfigurationFacet;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
 import org.jboss.forge.addon.projects.ProjectFactory;
 import org.jboss.forge.addon.projects.Projects;
 import org.jboss.forge.addon.ui.command.CommandExecutionListener;
@@ -9,8 +10,13 @@ import org.jboss.forge.addon.ui.command.UICommand;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.context.UISelection;
 import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
 
 import javax.inject.Inject;
+import java.io.FileNotFoundException;
+
+import static org.jboss.forge.addons.envers.commands.Constants.AUDITED_ANNOTATION;
+import static org.jboss.forge.addons.envers.commands.Constants.AUTO_AUDIT_CONFIG_ENTRY;
 
 /**
  * @author Ivan St. Ivanov
@@ -32,16 +38,24 @@ public class JpaEntityCreationListener implements CommandExecutionListener
                .getName();
       if (commandName.equals("JPA: New Entity") && projectFactory != null)
       {
-
          Configuration configuration = Projects
                   .getSelectedProject(projectFactory, uiExecutionContext.getUIContext())
                   .getFacet(ConfigurationFacet.class)
                   .getConfiguration();
-         if ((Boolean) configuration.getProperty(Constants.AUTO_AUDIT_CONFIG_ENTRY))
+         if (configuration.getBoolean(AUTO_AUDIT_CONFIG_ENTRY, false))
          {
-            uiExecutionContext.getUIContext().getSelection();
+            try {
+               JavaResource resource = (JavaResource) uiExecutionContext
+                        .getUIContext().getSelection().get();
+               JavaClassSource javaClass = resource.getJavaType();
+               if (!javaClass.hasAnnotation(AUDITED_ANNOTATION)) {
+                  javaClass.addAnnotation(AUDITED_ANNOTATION);
+               }
+               resource.setContents(javaClass);
+            } catch (FileNotFoundException fnfe) {
+               fnfe.printStackTrace();
+            }
          }
-
       }
    }
 
